9f64a43a1111a791d3e2e43c2c346fcd04618615
Yu HuiJi <117997919+chagok14@users.noreply.github.com>
2023-01-04 23:06:09 +0900

    Update seoul_map.py

--- pages/seoul_map.py
+++ pages/seoul_map.py
@@ -10,40 +10,11 @@ from folium.plugins import MarkerCluster
 
 from utils import info_desc
 '''
-import streamlit as st
-from utils import practice_desc as prac
-from utils import map_desc
-import folium
-from folium.plugins import MarkerCluster
-from streamlit_folium import st_folium
 
 def app():
     
     df = prac.prepare()
     map_desc.map_make()
-    
-    m = folium.Map(
-    location = [37.55, 126.98], zoom_start = 12)
-    '''
-    marker_cluster = MarkerCluster().add_to(m)
-    
-    for name, lat, lng, op in zip(df.문화시설명, df.위도, df.경도, df.주제분류_new):
-        if op == 0:
-            folium.Marker([lat, lng], popup = name, icon = folium.Icon(color = 'green')).add_to(marker_cluster)
-        elif op == 1:
-            folium.Marker([lat, lng], popup = name, icon = folium.Icon(color = 'red')).add_to(marker_cluster)
-        elif op == 2:
-            folium.Marker([lat, lng], popup = name, icon = folium.Icon(color = 'pink')).add_to(marker_cluster)
-        elif op == 3:
-            folium.Marker([lat, lng], popup = name, icon = folium.Icon(color = 'orange')).add_to(marker_cluster)
-        elif op == 4:
-            folium.Marker([lat, lng], popup = name, icon = folium.Icon(color = 'purple')).add_to(marker_cluster)
-        elif op == 5:
-            folium.Marker([lat, lng], popup = name, icon = folium.Icon(color = 'black')).add_to(marker_cluster)
-        elif op == 6:
-            folium.Marker([lat, lng], popup = name, icon = folium.Icon(color = 'blue')).add_to(marker_cluster)
-            '''
-
     st_data = st_folium(m)
     
     st.write('''
