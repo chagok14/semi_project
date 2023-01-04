508c5f8f755bef21512a72ef2aa443bba46a569d
Yu HuiJi <117997919+chagok14@users.noreply.github.com>
2023-01-04 22:49:10 +0900

    Update info_desc.py

--- utils/info_desc.py
+++ utils/info_desc.py
@@ -16,18 +16,12 @@ from utils import practice_desc as prac
 def showGraph():
     df = prac.prepare()
     plt.rcParams['axes.unicode_minus'] = False
-    count_result = df['구명'].value_counts()
-    df_sorted = df.sort_values(count_result, ascending = False)
-    fig = plotly.bar(df_sorted, x = '지역구', y = 'Count')
-    st.plotly_chart(fig)
-    '''
     plt.figure(figsize=(12, 30))
     sns.countplot(y=df['구명'], order=df['구명'].value_counts().index)
     plt.yticks(fontsize = 12)
     plt.title('구별 문화공간 개수', size = 18)
     st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot()
-    '''
 
 def showSubGraph():
     df = prac.prepare()
